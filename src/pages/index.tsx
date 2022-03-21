7fd4698faa4118d881f09714eeb7525040be078e
frontend <gabriellennon@wizsolucoes.com.br>
2022-03-21 16:14:17 -0300

    configuration global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hello World!</h1>
+      <h1>Hello World!</h1>
     </>
   )
 }
